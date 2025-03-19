df3f27efd3f7deeea4c0cdef215f4ae1eb56c16c
Moussadoucoure <99295247+Moussadoucoure@users.noreply.github.com>
2025-03-19 12:50:01 -0400

    arriere plan #2

--- ArrierePlan.js
+++ ArrierePlan.js
@@ -1,112 +1,76 @@
-class ArrierePlan{
-  constructor(scene){
+class ArrierePlan {
+  constructor(scene) {
     this.scene = scene;
     this.conteneur = new createjs.Container();
 
-    this.estChargePaysageArriere = false;
-    this.shapePaysageArriere = new createjs.Shape();
-    this.matricePaysageArriere = new createjs.Matrix2D();
-    this.imagePaysageArriere = new Image();
-    this.imagePaysageArriere.addEventListener("load", evenementloadpaysagearriere => this.creerShapePaysageArriere(evenementloadpaysagearriere));
-    this.imagePaysageArriere.src = "illustration/maisonpelouse7.png"
+    // Initialisation des variables
+    this.images = {
+      paysageArriere: new Image(),
+      paysageAvant: new Image(),
+      paysageSol: new Image(),
+    };
 
-    this.estChargePaysageAvant = false;
-    this.shapePaysageAvant = new createjs.Shape();
-    this.matricePaysageAvant = new createjs.Matrix2D();
-    this.imagePaysageAvant = new Image();
-    this.imagePaysageAvant.addEventListener("load", evenementloadpaysageavant => this.creerShapePaysageAvant(evenementloadpaysageavant));
-    this.imagePaysageAvant.src = "illustration/arrierePlan2.png"
+    this.shapes = {
+      paysageArriere: new createjs.Shape(),
+      paysageAvant: new createjs.Shape(),
+      paysageSol: new createjs.Shape(),
+    };
 
-    this.estChargePaysageSol = false;
-    this.shapePaysageSol = new createjs.Shape();
-    this.matricePaysageSol = new createjs.Matrix2D();
-    this.imagePaysageSol = new Image();
-    this.imagePaysageSol.addEventListener("load", evenementloadpaysagesol => this.creerShapePaysageSol(evenementloadpaysagesol));
-    this.imagePaysageSol.src = "illustration/sol5.png"
+    this.matrices = {
+      paysageArriere: new createjs.Matrix2D(),
+      paysageAvant: new createjs.Matrix2D(),
+      paysageSol: new createjs.Matrix2D(),
+    };
 
+    // Chargement des images avec des écouteurs d'événements
+    this.images.paysageArriere.src = "illustration/maisonpelouse7.png";
+    this.images.paysageAvant.src = "illustration/arrierePlan2.png";
+    this.images.paysageSol.src = "illustration/sol5.png";
 
-
+    this.images.paysageArriere.addEventListener("load", () => this.creerShape("paysageArriere"));
+    this.images.paysageAvant.addEventListener("load", () => this.creerShape("paysageAvant"));
+    this.images.paysageSol.addEventListener("load", () => this.creerShape("paysageSol"));
   }
 
-  creerShapePaysageArriere(evenementloadpaysagearriere){
+  // Crée un shape générique pour les différents éléments
+  creerShape(type) {
+    const image = this.images[type];
+    const shape = this.shapes[type];
+    const matrix = this.matrices[type];
 
-    this.shapePaysageArriere.graphics.beginBitmapFill(this.imagePaysageArriere, "repeat", this.matricePaysageArriere).drawRect(0,0,1919,914).endStroke();
-    console.log("creerShapePaysageArriere");
-    this.estChargePaysageArriere = true;
+    shape.graphics.beginBitmapFill(image, "repeat", matrix).drawRect(0, 0, 1919, 914).endStroke();
+    console.log(`Shape ${type} créé`);
+    this[`estCharge${type.charAt(0).toUpperCase() + type.slice(1)}`] = true;
   }
 
-  creerShapePaysageAvant(evenementloadpaysageavant){
-
-    this.shapePaysageAvant.graphics.beginBitmapFill(this.imagePaysageAvant, "repeat", this.matricePaysageAvant).drawRect(0,0,1919,914).endStroke();
-    this.estChargePaysageAvant = true;
+  // Vérifie si tous les éléments sont chargés
+  get estCharge() {
+    return Object.values(this.images).every(image => image.complete);
   }
 
-  creerShapePaysageSol(evenementloadpaysagesol){
-
-    this.shapePaysageSol.graphics.beginBitmapFill(this.imagePaysageSol, "repeat", this.matricePaysageSol).drawRect(0,0,1919,914).endStroke();
-    this.estChargePaysageSol = true;
+  // Affiche tous les éléments du paysage
+  afficher() {
+    this.conteneur.addChild(this.shapes.paysageAvant);
+    this.conteneur.addChild(this.shapes.paysageArriere);
+    this.conteneur.addChild(this.shapes.paysageSol);
+    this.conteneur.name = "arriere";
+    this.scene.addChild(this.conteneur);
   }
 
-
-
-  get estCharge(){
-    return this.estChargePaysageArriere && this.estChargePaysageAvant && this.estChargePaysageSol;
-  }
-
-  afficher(){
-   this.conteneur.addChild(this.shapePaysageAvant);
-   this.conteneur.addChild(this.shapePaysageArriere);
-
-   this.conteneur.addChild(this.shapePaysageSol);
-   this.conteneur.name = "arriere";
-   this.scene.addChild(this.conteneur);
-  }
-
-
-  traiter(demande){
-    switch(demande){
+  // Traite les demandes d'animation
+  traiter(demande) {
+    switch (demande) {
       case ArrierePlan.DEMANDE.ATTENDRE:
-        this.etat = ArrierePlan.DEMANDE.ATTENDRE;
+        this.etat = ArrierePlan.ETAT.ATTENTE;
         break;
       case ArrierePlan.DEMANDE.ALLER_A_DROITE:
-        this.etat = ArrierePlan.DEMANDE.MOUVEMENT_A_DROITE;
+        this.etat = ArrierePlan.ETAT.MOUVEMENT_A_DROITE;
         break;
-
-
-    }
-
-  }
-
-  animer(secondeEcoulee){
-    this.matricePaysageAvant.translate(-ArrierePlan.VITESSE_PIXEL_SECONDE.PAYSAGE_AVANT * secondeEcoulee, 0);
-    this.matricePaysageArriere.translate(-ArrierePlan.VITESSE_PIXEL_SECONDE.PAYSAGE_ARRIERE * secondeEcoulee, 0);
-    switch(this.etat){
-      case ArrierePlan.ETAT.ATTENTE:
-        break;
-
-      case ArrierePlan.ETAT.MOUVEMENT_A_DROITE:
-        this.matricePaysageSol.translate(-ArrierePlan.VITESSE_PIXEL_SECONDE.SOL * secondeEcoulee, 0);
-        break;
-
     }
   }
 
-}
-ArrierePlan.DEMANDE = {
-  ATTENDRE : 0,
-  MOUVEMENT_A_DROITE : 1
-
-}
-
-ArrierePlan.ETAT = {
-  ATTENTE : 0,
-  MOUVEMENT_A_DROITE : 1
-
-}
-
-ArrierePlan.VITESSE_PIXEL_SECONDE = {
-  PAYSAGE_ARRIERE : 15,
-  PAYSAGE_AVANT : 50,
-  SOL : 70
-
-}
+  // Anime l'arrière-plan en fonction de l'état et du temps écoulé
+  animer(secondeEcoulee) {
+    // Déplacement des éléments en fonction des vitesses spécifiées
+    this.matrices.paysageAvant.translate(-ArrierePlan.VITESSE_PIXEL_SECONDE.PAYSAGE_AVANT * secondeEcoulee, 0);
+    this.matrices.paysageArriere.translate(-ArrierePlan.VITESSE_PIXEL_SECON
